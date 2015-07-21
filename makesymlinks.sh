6a45889cf07d9a38564676c001fa78ea42cffa27
Kia Fathi <kia.fathi@minted.com>
2015-07-21 10:51:04 -0700

    (feat) Add vim directory and .gitignore

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 ############################
 # .make.sh
-# This script creates symlinks from the home directory to any desired dotfiles in ~/dotfiles
+# This script creates symlinks from the home directory to any desired dotfiles 
+# in ~/dotfiles
 ############################
 
 ########## Variables
 
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
-files="bashrc vimrc vim zshrc oh-my-zsh"    # list of files/folders to symlink in homedir
+files="vimrc vim"                 # list of files/folders to symlink in homedir
 
 ##########
 
@@ -22,7 +23,8 @@ echo "Changing to the $dir directory"
 cd $dir
 echo "...done"
 
-# move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks 
+# move any existing dotfiles in homedir to dotfiles_old directory, then create 
+# symlinks 
 for file in $files; do
     echo "Moving any existing dotfiles from ~ to $olddir"
     mv ~/.$file ~/dotfiles_old/
